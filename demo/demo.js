e799154dc4656416f5c656789dea43fa7ace17a5
Massimo Donati <madonati@deloitte.com>
2014-03-06 17:16:28 -0800

    accelerometer

--- demo/demo.js
+++ demo/demo.js
@@ -1,30 +1,32 @@
-var L3GD20, LPS331, compass, curMeasurements, measure, measurements, prSensor, timer;
+var L3GD20, LPS331, LSM303, compass, curMeasurements, measure, measurements, prSensor, timer;
 
-var i2c = require('i2c');
-var address = 0x18;
-var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
+// var i2c = require('i2c');
+// var address = 0x18;
+// var wire = new i2c(address, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 
 LPS331 = require('../lib/LPS331');
-
+LSM303 = require('../lib/LSM303');
 L3GD20 = require('../lib/L3GD20');
 
-prSensor = new LPS331(wire, null, null, true);
-
+// prSensor = new LPS331(wire, null, null, true);
 // compass = new L3GD20();
+accelerometer = new LSM303(null, null, true);
 
-prSensor.setResolution(9);
+// prSensor.setResolution(9);
 
 measure = function() {
   console.log("---------------*****---------------");
 
-  console.log("---------------*****---------------");
-  console.log("Reading Temperature");
-  prSensor.readTemperature();
-  console.log("---------------");
-  console.log("Reading Pressure");
-  prSensor.readPressure();
+  // console.log("---------------*****---------------");
+  // console.log("Reading Temperature");
+  // prSensor.readTemperature();
+  // console.log("---------------");
+  // console.log("Reading Pressure");
+  // prSensor.readPressure();
+  accelerometer.readAcc();
   // console.log("-------Reading axes--------");
   // compass.readAllAxes();
+
   console.log(" ");
   if (measurements > 0) {
     curMeasurements++;
@@ -34,11 +36,11 @@ measure = function() {
   }
 };
 
-measurements = 5;
+measurements = 0;
 
 curMeasurements = 0;
 
-timer = setInterval(measure, 2000);
+timer = setInterval(measure, 100);
 
 
 // wire.scan(function(err, data) {
